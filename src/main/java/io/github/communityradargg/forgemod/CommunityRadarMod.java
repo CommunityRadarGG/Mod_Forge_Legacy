15ccc7151dc2ded81bdc141f12a6c98d7e4b2a11
BlockyTheDev <86119630+BlockyTheDev@users.noreply.github.com>
2024-06-16 17:51:55 +0200

    Back to snapshot for development

--- src/main/java/io/github/communityradargg/forgemod/CommunityRadarMod.java
+++ src/main/java/io/github/communityradargg/forgemod/CommunityRadarMod.java
@@ -41,7 +41,7 @@ public class CommunityRadarMod {
     /** The id of the mod. */
     public static final String MODID = "communityradargg";
     /** The version of the mod. */
-    public static final String VERSION = "1.1.0-1.8.9";
+    public static final String VERSION = "1.1.1-1.8.9-SNAPSHOT";
     private static final Logger logger = LogManager.getLogger(CommunityRadarMod.class);
     private static RadarListManager listManager;
     private boolean onGrieferGames = false;
